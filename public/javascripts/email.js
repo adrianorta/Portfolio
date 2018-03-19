9da0c4b88a0de1a4254cc87a6cfd7652b096c497
adrianorta <adrianorta510@gmail.com>
2018-03-19 04:10:45 -0400

    remove from mobile

--- public/javascripts/email.js
+++ public/javascripts/email.js
@@ -1,12 +1,14 @@
 let subject = '',
     message = '';
 $('form').submit(function(){
-  if($('#subject').val() !== '') {
-    subject = $('#subject').val();
-  } else {
-    subject = 'Contact Form'
-  }
+  if(!(/Android|webOS|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i.test(navigator.userAgent) )) {
+    if($('#subject').val() !== '') {
+      subject = $('#subject').val();
+    } else {
+      subject = 'Contact Form'
+    }
 
-  message = $('#message').val();
-  $('form').attr('action', `mailto:adrianorta510@gmail.com?subject=${subject}&body=${message}`);
+    message = $('#message').val();
+    $('form').attr('action', `mailto:adrianorta510@gmail.com?subject=${subject}&body=${message}`);
+  }
 })
